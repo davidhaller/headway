0d8c9c2d7369c9f83d639a95df041f08e36c0158
David Haller <davidhaller@mailbox.org>
2022-07-29 18:09:55 +0200

    fix macro evaluation in config.hpp

--- config.hpp
+++ config.hpp
@@ -6,15 +6,15 @@ namespace Headway
     constexpr int VERSION_MAJOR = 4;
     constexpr int VERSION_MINOR = 1;
 
-#ifdef Q_OS_MACOS
+#if defined(Q_OS_MACOS)
     constexpr bool GLOBAL_MENUBAR = true;
 #else
     constexpr bool GLOBAL_MENUBAR = false;
 #endif
 
-#ifdef Q_OS_WINDOWS
+#if defined(Q_OS_WINDOWS)
     constexpr auto QUICK_CONTROLS_STYLE = "Universal";
-#elif Q_OS_MACOS
+#elif defined(Q_OS_MACOS)
     constexpr auto QUICK_CONTROLS_STYLE = "macOS";
 #else
     constexpr auto QUICK_CONTROLS_STYLE = "Basic";
